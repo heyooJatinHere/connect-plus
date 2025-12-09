f35d19b0d7434d44826fd6b175fef6b1d13ed33a
Jatin Rajput <jatinrjpt21@gmail.com>
2025-12-09 23:44:39 +0530

    use VITE_API_URL

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // change to Render URL after deployment
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", // change to Render URL after deployment
 });
 
 // signup
